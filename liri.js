96d15dbd49101d3e4b9f561c0641c4dc71156d8f
Eki <ekitaula@gmail.com>
2019-01-05 18:18:47 -0600

    last commit

--- liri.js
+++ liri.js
@@ -35,12 +35,12 @@ if (process.argv[2] == 'concert-this') {
 
         var tableArray = [];
 
-        for (var i = 0; i < data.track.items.length; i++ ) {
+        for (var i = 0; i < data.tracks.items.length; i++ ) {
             var result = {
-                artist : data.tracks.items[0].album.artists[0].name,
-                album_name : data.tracks.items[0].album.name,
-                song_name : data.tracks.items[0].name,
-                preview_url : data.tracks.items[0].preview_url
+                artist : data.tracks.items[i].album.artists[0].name,
+                album_name : data.tracks.items[i].album.name,
+                song_name : data.tracks.items[i].name,
+                preview_url : data.tracks.items[i].preview_url
             }
             tableArray.push(result);
         }
